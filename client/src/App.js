31c076f339faa5525f17c3dfca387886204cd173
vanbietnguyen <vanbietnguyen@gmail.com>
2021-12-25 00:55:39 -0500

    sign in form

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,17 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Homepage from './pages/Homepage'
 import ShopPage from './pages/shoppage/ShopPage.container'
+import Header from './components/header/Header'
+import Auth from './pages/Auth'
 
 function App() {
   return (
     <div >
+      <Header />
       <Routes>
         <Route exact path="/" element={<Homepage />}/>
-        <Route exact path="/shop" element={<ShopPage />}/>
+        <Route path="/shop" element={<ShopPage />}/>
+        <Route path='/signin' element={<Auth />} />
       </Routes>
     </div>
   );
---
+++ client/src/components/auth/SignIn.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import FormInput from './FormInput';
+import CustomButton from './CustomButton';
+
+
+const SignIn = () => {
+  
+  const [ email, setEmail ] = useState('')
+  const [ password, setPassword ] = useState('')
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setEmail('')
+    setPassword('')
+  };
+
+  const handleChange = event => {
+    const { value, name } = event.target;
+    if(name === 'email') setEmail(value)
+    else setPassword(value)
+  };
+
+
+  return (
+    <div className='sign-in'>
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
+
+        <form onSubmit={handleSubmit}>
+          <FormInput
+            name='email'
+            type='email'
+            handleChange={handleChange}
+            value={email}
+            label='email'
+            required
+          />
+          <FormInput
+            name='password'
+            type='password'
+            value={password}
+            handleChange={handleChange}
+            label='password'
+            required
+          />
+          <CustomButton type='submit'> Sign in </CustomButton>
+        </form>
+      </div>
+  )
+
+}
+
+export default SignIn;
